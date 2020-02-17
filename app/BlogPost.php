6031798c26b34404fc76fc11f1007537dfa11522
Akash Rumi <48693527+akash-rumi@users.noreply.github.com>
2020-02-17 15:05:41 +0600

    soft delete and restore in DB

--- app/BlogPost.php
+++ app/BlogPost.php
@@ -3,14 +3,30 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class BlogPost extends Model
 {
+    use SoftDeletes;
+
     protected $table = 'blogposts';
     protected $fillable = ['title', 'content'];
 
     public function comments()
     {
         return $this->hasMany('App\comment');
+    }
+
+    public static function boot()
+    {
+        parent::boot();
+
+        static::deleting(function (BlogPost $blogPost) {
+            $blogPost->comments()->delete();
+        });
+
+        static::restoring(function (BlogPost $blogPost) {
+            $blogPost->comments()->restore();
+        });
     }
 }
