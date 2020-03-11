79dbcd9c5b07859c39c92d0d894321e35e0534ec
Akash Rumi <48693527+akash-rumi@users.noreply.github.com>
2020-03-11 09:22:02 +0600

    send email using markdown template.

--- app/Http/Controllers/PostCommentController.php
+++ app/Http/Controllers/PostCommentController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\BlogPost;
 use App\Http\Requests\StoreComment;
 use App\Mail\CommentPosted;
+use App\Mail\CommentPostedMarkdown;
 use Illuminate\Support\Facades\Mail;
 
 class PostCommentController extends Controller
@@ -22,7 +23,7 @@ class PostCommentController extends Controller
             'user_id' => $request->user()->id
         ]);
 
-        Mail::to($post->user)->send(new CommentPosted($comment));
+        Mail::to($post->user)->send(new CommentPostedMarkdown($comment));
         return redirect()->back()->withStatus('Comment was created!');
     }
 }
